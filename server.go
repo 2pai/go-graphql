9ebafb7a54bfea4c13313487675b118ff6121c78
2pai <iqbalsyamilayas@gmail.com>
2020-04-13 21:38:17 +0700

    init
    Signed-off-by: 2pai <iqbalsyamilayas@gmail.com>

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/2pai/go-graphql/graph"
+	"github.com/2pai/go-graphql/graph/generated"
+	"github.com/2pai/go-graphql/internal/pkg/db/mysql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	database.InitDB()
+	database.Migrate()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
