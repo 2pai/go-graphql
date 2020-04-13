f0ea262682c1bd4f456e5cf606868936647068b2
2pai <iqbalsyamilayas@gmail.com>
2020-04-13 21:40:36 +0700

    add users service
    Signed-off-by: 2pai <iqbalsyamilayas@gmail.com>

---
+++ internal/users/users.go
@@ -0,0 +1,35 @@
+package users
+
+import (
+	database "github.com/2pai/go-graphql/internal/pkg/db/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"name"`
+	Password string `json:"password"`
+}
+
+func (user *User) Create() {
+	statement, err := database.DB.Prepare("INSERT INTO (Username,Password) VALUES(?,?)")
+	if err != nil {
+		log.Panic(err)
+	}
+	hashedPassword, err := HashPassword(user.Password)
+	_, err = statement.Exec(user.Username, hashedPassword)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(hashedPassword), err
+}
+
+func VerifyPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
